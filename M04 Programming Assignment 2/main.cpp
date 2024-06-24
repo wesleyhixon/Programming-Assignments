7ba9912cc9e505f5c582deb2ba0007015c050ddb
wesleyhixon <wesleyhixon@gmail.com>
2024-06-24 15:48:27 -0400

    Files for new assignment

---
+++ M04 Programming Assignment 2/main.cpp
@@ -0,0 +1,3 @@
+cout << "Welcome to the binary number converter 2.0." << endl; 
+cout << "You will enter a binary number 1 digit at a time starting at the right and the program will convert the number to decimal." << endl;
+    
