9ee87bc75acac230b2f042cb1fbbfe1461eb1b97
wesleyhixon <wesleyhixon@gmail.com>
2024-07-20 00:03:20 -0400

    new file:   M08 Final Project/main.cpp

---
+++ M08 Final Project/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <math.h>
+
+/*
+Name: System of Linear Equations
+Author: Wesley Hixon
+Due Date: 07/25/2024
+Purpose: Make a program which solves a system of linear equations given 2 lines of form Ax+By=C
+*/
+
