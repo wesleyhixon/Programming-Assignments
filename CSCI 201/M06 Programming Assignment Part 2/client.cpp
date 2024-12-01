f49707a268716488f19d0e94d8485f8ed319cf00
wesleyhixon <wesleyhixon@gmail.com>
2024-11-30 19:24:19 -0500

    new file:   CSCI 201/M06 Programming Assignment Part 2/SampleSubmission.docx 	modified:   CSCI 201/M06 Programming Assignment Part 2/client 	modified:   CSCI 201/M06 Programming Assignment Part 2/client.cpp 	modified:   CSCI 201/M06 Programming Assignment Part 2/server 	modified:   CSCI 201/M06 Programming Assignment Part 2/server.cpp

--- CSCI 201/M06 Programming Assignment Part 2/client.cpp
+++ CSCI 201/M06 Programming Assignment Part 2/client.cpp
@@ -4,27 +4,112 @@
 #include <netdb.h>
 #include <cstring>
 #include <unistd.h>
-
+#include <array>
+#include <cstring>
+#include <algorithm>
+#include <arpa/inet.h>
 
 using namespace std;
 
 int main(){
+
+    // Array of state abbreviations
+    array<string, 50> stateAbbreviations = {
+        "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"
+    };
+
+    // Getting abbreviation
+    string abbreviation;
+    cout << "Please enter a state abbreviation: ";
+    bool valid = false;
+    while(!valid){
+        try{
+            // Get input
+            cin >> abbreviation;
+            if(!cin) throw(runtime_error("Please enter a valid 2-letter state abbreviation"));
+
+            // Convert abbreviation to uppercase
+            transform(abbreviation.begin(), abbreviation.end(),abbreviation.begin(), ::toupper);
+
+            // Checking against list of abbreviations
+            for(int i = 0; i < 50; i++){
+                if(abbreviation == stateAbbreviations[i]){
+                    valid = true;
+                }
+            }
+            // If invalid
+            if(!valid) throw(runtime_error("Please enter a valid 2-letter state abbreviation"));
+        }catch(const exception& e){
+            cerr << e.what() << endl;
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+    }
+
+    // Client code
     try{
-    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080);
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
+        // Creating new socket
+        int sock = socket(AF_INET, SOCK_STREAM, 0);
 
-    connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+        if(sock < 0){
+            throw(runtime_error("Failure to create socket."));
+        }
+        
+        // Building server address
+        sockaddr_in serverAddress;
+        serverAddress.sin_family = AF_INET;
+        serverAddress.sin_port = htons(8080);
 
-    const char* message = "Hello, server!";
-    send(clientSocket, message, strlen(message), 0);
+        // Get IP address for server
+        valid = false;
+        while(!valid){
+            string address;
+            cout << "Please enter the server IPv4 address. Enter 127.0.0.1 for localhost: ";
+            cin >> address;
 
-    close(clientSocket);
+            // Throws error if fails to set server IPv4 address
+            if(inet_pton(AF_INET, address.c_str(), &serverAddress.sin_addr) <= 0){
+                cerr << "Invalid IP address. Please try again." << endl;
+                continue;
+            }
+            valid = true;
+        }
+
+        // Attempt to connect to server
+        int connectionStatus = connect(sock, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+
+        if(connectionStatus < 0){
+            throw(runtime_error("Failure to connect."));
+        }
+
+        // Convert abbreviation to cstring and send to server
+        const char* message = abbreviation.c_str();
+        int messageSent = send(sock, message, abbreviation.size(), 0);
+
+        if(messageSent < 0){
+            throw(runtime_error("Message failed to send."));
+        }
+
+        // Recieving message from server
+        char buffer[1024] = {0};
+        int bytesRead = recv(sock, buffer, sizeof(buffer), 0);
+
+        if(bytesRead < 0){
+            throw(runtime_error("Error receiving message."));
+        }
+        if(bytesRead == 0){
+            throw(runtime_error("No message received."));
+            
+        }
+
+        // Outputting message
+        cout << "Message from server: " << endl << buffer << endl;
+
+        // Closing connection and exiting
+        close(sock);
     }catch(const exception& e){
         cerr << e.what();
+        return -1;
     }
     return 0;
 }
--- CSCI 201/M06 Programming Assignment Part 2/server.cpp
+++ CSCI 201/M06 Programming Assignment Part 2/server.cpp
@@ -2,24 +2,126 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netdb.h>
-
-// https://www.geeksforgeeks.org/socket-programming-in-cpp/
+#include <unistd.h>
+#include <cstring>
+#include <string>
+#include <map>
 
 using namespace std;
 
+// Map of abbreviations to long names
+map<string, string> stateMap{
+    {"AL", "Alabama"},
+    {"AK", "Alaska"},
+    {"AZ", "Arizona"},
+    {"AR", "Arkansas"},
+    {"CA", "California"},
+    {"CO", "Colorado"},
+    {"CT", "Connecticut"},
+    {"DE", "Delaware"},
+    {"FL", "Florida"},
+    {"GA", "Georgia"},
+    {"HI", "Hawaii"},
+    {"ID", "Idaho"},
+    {"IL", "Illinois"},
+    {"IN", "Indiana"},
+    {"IA", "Iowa"},
+    {"KS", "Kansas"},
+    {"KY", "Kentucky"},
+    {"LA", "Louisiana"},
+    {"ME", "Maine"},
+    {"MD", "Maryland"},
+    {"MA", "Massachusetts"},
+    {"MI", "Michigan"},
+    {"MN", "Minnesota"},
+    {"MS", "Mississippi"},
+    {"MO", "Missouri"},
+    {"MT", "Montana"},
+    {"NE", "Nebraska"},
+    {"NV", "Nevada"},
+    {"NH", "New Hampshire"},
+    {"NJ", "New Jersey"},
+    {"NM", "New Mexico"},
+    {"NY", "New York"},
+    {"NC", "North Carolina"},
+    {"ND", "North Dakota"},
+    {"OH", "Ohio"},
+    {"OK", "Oklahoma"},
+    {"OR", "Oregon"},
+    {"PA", "Pennsylvania"},
+    {"RI", "Rhode Island"},
+    {"SC", "South Carolina"},
+    {"SD", "South Dakota"},
+    {"TN", "Tennessee"},
+    {"TX", "Texas"},
+    {"UT", "Utah"},
+    {"VT", "Vermont"},
+    {"VA", "Virginia"},
+    {"WA", "Washington"},
+    {"WV", "West Virginia"},
+    {"WI", "Wisconsin"},
+    {"WY", "Wyoming"}
+};
+
 int main(){
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    // Create socket for server
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
     
+    // Create address for socket
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(8080);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
-    listen(serverSocket, 5);
-    int clientSocket = accept(serverSocket, nullptr, nullptr);
-    
-    char buffer[1024] = {0};
-    recv(clientSocket, buffer, sizeof(buffer), 0);
-    cout << "Message from client: " << buffer << endl;
+    // Bind address to socket
+    bind(sock, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+
+    // Wait for connection on socket
+    listen(sock, 5);
+
+    // Runs indefinitely
+    while(true){
+        // Accepts new connection
+        int clientSocket = accept(sock, nullptr, nullptr);
+        
+        // Receive message from client
+        char buffer[1024] = {0};
+        int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+
+        if(bytesReceived < 0){
+            cerr << "Message not received" << endl;
+        }
+
+        // Outputting message from client
+        cout << "Message from client: " << buffer << endl;
+
+
+        string abbreviationString(buffer);
+        string longName;
+
+        // Finding long name for abbreviation
+        for(auto itr = stateMap.begin(); itr != stateMap.end(); itr++){
+            if(itr->first == buffer){
+                longName = itr->second;
+                break;
+            }
+            else{
+                longName = "Abbreviation not found!";
+            }
+        }
+
+
+        string message = "Here is your abbreviation in long form: " + longName;
+
+
+        // Converting message to cstring and sending to client
+        int messageSent = send(clientSocket, message.c_str(), message.length(), 0);
+
+        if(messageSent < 0){
+            cerr << "Message not sent" << endl;
+        }
+
+        // Closing socket
+        close(clientSocket);
+    }
 }
